b5c0fd60c30251fd89a293eb55faec65c93a3a2e
Roman Votyakov <votjakovr@gmail.com>
2012-12-12 12:57:58 +0700

    Dummy test_gibdd_system_db.sql were added

---
+++ sql/test_gibdd_system_db.sql
@@ -0,0 +1,14 @@
+/*
+ * Model: gibdd-system
+ * Database: MySQL 5.5
+ */
+
+/*
+ * This is SQL script for filling database with some test data.
+ * It should contain something like this:
+ * INSERT INTO table1_name VALUES (value1, value2, ...);
+ * ...
+ * INSERT INTO table2_name VALUES (value1, value2, ...);
+ * ... 
+ * etc.
+ */
