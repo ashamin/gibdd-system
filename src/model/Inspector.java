9fe254b187e3d15b48ffa609b6a91f43b5348c93
Anton Shamin <anthony.shamin@gmail.com>
2012-12-13 15:01:21 +0600

    Insert update delete select toString methods was added to Inspector class.

--- src/model/Inspector.java
+++ src/model/Inspector.java
@@ -1,5 +1,10 @@
 ﻿package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Inspector Класс, включающий основные свойства инспектора ГИБДД. Содержит
  * информацию о звании, должности инспектора, а также логин и пароль,
@@ -124,32 +129,196 @@ public class Inspector extends Human {
 	 */
 	@Override
 	public void insert() {
-		// TODO implement database insert operation
-		throw new UnsupportedOperationException("not implemented");
+		try {
+			Connection conn = this.getConnection();
+			try {
+				PreparedStatement stmtIns = conn.prepareStatement(
+						"insert into gibdd_system_db.inspectors (inspector_id, login, password, "
+								+ "human_id, rank_id, post_id)"
+								+ " values (default, ?, ?, ?, ?, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+				stmtIns.setString(1, this.login);
+				stmtIns.setString(2, this.password);
+
+				Human tmp = new Human((Human) this);
+				tmp.insert();
+				stmtIns.setInt(3, tmp.id);
+
+				PreparedStatement stmtSlct = conn
+						.prepareStatement("select rank_id from gibdd_system_db.ranks where "
+								+ "rank=?");
+				stmtSlct.setString(1, this.rank);
+				ResultSet res = stmtSlct.executeQuery();
+				while (res.next())
+					stmtIns.setInt(4, res.getInt(1));
+
+				stmtSlct = conn
+						.prepareStatement("select post_id from gibdd_system_db.posts"
+								+ " where post=?");
+				stmtSlct.setString(1, this.post);
+				res = stmtSlct.executeQuery();
+				while (res.next())
+					stmtIns.setInt(5, res.getInt(1));
+
+				stmtIns.executeUpdate();
+
+				ResultSet key = stmtIns.getGeneratedKeys();
+				key.next();
+				this.id = key.getInt(1);
+
+				System.out.println("...Row with string representation \n\t"
+						+ this.toString() + "\nwas added");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void update() {
-		// TODO implement database update operation
-		throw new UnsupportedOperationException("not implemented");
+		try {
+			Inspector tmp = new Inspector(this);
+			tmp.select(this.id);
+			String oldRepr = tmp.toString();
+
+			Connection conn = this.getConnection();
+			try {
+				PreparedStatement stmt = conn
+						.prepareStatement("update gibdd_system_db.humans, gibdd_system_db.inspectors set"
+								+ " humans.name=?,"
+								+ " humans.passport_number=?, "
+								+ "humans.address=?, inspectors.login=?, inspectors.password=?, "
+								+ "inspectors.rank_id=?, inspectors.post_id=? "
+								+ "where inspectors.inspector_id = "
+								+ Integer.toString(this.id)
+								+ " and "
+								+ "humans.human_id = inspectors.human_id");
+				stmt.setString(1, this.name);
+				stmt.setString(2, this.passportNumber);
+				stmt.setString(3, this.address);
+				stmt.setString(4, this.login);
+				stmt.setString(5, this.password);
+
+				PreparedStatement stmtSlct = conn
+						.prepareStatement("select rank_id from gibdd_system_db.ranks where "
+								+ "rank=?");
+				stmtSlct.setString(1, this.rank);
+				ResultSet res = stmtSlct.executeQuery();
+				while (res.next())
+					stmt.setInt(6, res.getInt(1));
+
+				stmtSlct = conn
+						.prepareStatement("select post_id from gibdd_system_db.posts"
+								+ " where post=?");
+				stmtSlct.setString(1, this.post);
+				res = stmtSlct.executeQuery();
+				while (res.next())
+					stmt.setInt(7, res.getInt(1));
+
+				stmt.executeUpdate();
+
+				System.out
+						.println("...Row in base with string representation \n\t"
+								+ oldRepr
+								+ "\nwas updated to\n\t"
+								+ this.toString());
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void delete() {
-		// TODO implement database delete operation
-		throw new UnsupportedOperationException("not implemented");
+		try {
+			Connection conn = this.getConnection();
+			try {
+				PreparedStatement stmt = conn
+						.prepareStatement("delete from gibdd_system_db.inspectors where inspector_id = "
+								+ Integer.toString(this.id));
+				stmt.executeUpdate();
+
+				System.out.println("...Row with string representation \n\t"
+						+ this.toString() + "\nwas deleted from base");
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void select(int id) {
-		// TODO implement database select operation
-		throw new UnsupportedOperationException("not implemented");
+		try {
+			Connection conn = this.getConnection();
+			try {
+				PreparedStatement stmt = conn
+						.prepareStatement("select humans.name, humans.passport_number, "
+								+ "humans.address, "
+								+ "inspectors.inspector_id, inspectors.login, inspectors.password, "
+								+ "ranks.rank, posts.post "
+								+ "from gibdd_system_db.humans, gibdd_system_db.inspectors, "
+								+ "gibdd_system_db.ranks, gibdd_system_db.posts "
+								+ "where "
+								+ "inspectors.inspector_id = "
+								+ Integer.toString(id)
+								+ " and "
+								+ "humans.human_id = inspectors.human_id and "
+								+ "ranks.rank_id = inspectors.rank_id and "
+								+ "posts.post_id = inspectors.post_id");
+				ResultSet res = stmt.executeQuery();
+
+				while (res.next()) {
+					this.id = res.getInt(4);
+					this.name = res.getString(1);
+					this.passportNumber = res.getString(2);
+					this.address = res.getString(3);
+					this.login = res.getString(5);
+					this.password = res.getString(6);
+					this.rank = res.getString(7);
+					this.post = res.getString(8);
+				}
+
+				System.out.println("...Row with string representation \n\t"
+						+ this.toString() + "\nwas selected from base");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public String toString() {
-		// TODO implement string representation of the object
-		throw new UnsupportedOperationException("not implemented");
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(", rank = " + this.rank + ", ");
+		sb.append("post = " + this.post + ", ");
+		sb.append("login = " + this.login + ", ");
+		sb.append("password = " + this.password);
+		return sb.toString();
 	}
 
 	/**
